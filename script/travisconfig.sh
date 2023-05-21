ae6ad10150ebb26b0c391fbfa825054601cef07c
Travis Vallon <travisnormanv@gmail.com>
2023-05-21 11:08:36 +0900

    polybar configuration and launch file

--- script/travisconfig.sh
+++ script/travisconfig.sh
@@ -21,10 +21,20 @@ if [ -f ~/.config/i3/config ]; then
 	mv ~/.config/i3/config /usr/local/share/backups/.config/i3/config
 fi
 
+if [ -f ~/.config/polybar/config.ini ]; then
+	mv ~/.config/polybar/config.ini /usr/local/share/backups/.config/polybar/config.ini
+fi
+
+if [ -f ~/.config/polybar/launch.sh ]; then
+	mv ~/.config/polybar/launch.sh /usr/local/share/backups/.config/polybar/launch.sh
+fi
+
 ## Now delete the configs
 rm -f ~/.Xresources
 rm -f ~/.vimrc
 rm -f ~/.config/i3/config
+rm -f ~/.config/polybar/config.ini
+rm -f ~/.config/polybar/launch.sh
 rm -f ~/.xsession
 rm -f ~/.xinitrc
 
@@ -35,3 +45,7 @@ ln -s /usr/local/share/travisconfig/.xsession ~/.xsession
 
 mkdir -p ~/.config/i3
 ln -s /usr/local/share/travisconfig/i3/config ~/.config/i3/config
+
+mkdir -p ~/.config/polybar
+ln -s /usr/local/share/travisconfig/polybar/config.ini ~/.config/polybar/config.ini
+ln -s /usr/local/share/travisconfig/polybar/launch.sh ~/.config/polybar/launch.sh
